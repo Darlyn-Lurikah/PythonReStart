0436fb29ca9f29e405fb24a5f9ac5705b62fb38e
Darlyn-Lurikah <norlaydee@hotmail.co.uk>
2023-03-18 12:12:28 +0000

    init commit for the AWS restart repo

---
+++ string_data.py
@@ -0,0 +1,20 @@
+
+myString = "This a string babes!"
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+
+firstString = "cute"
+secondString  = "babes"
+thirdString = firstString + secondString
+print(thirdString)
+
+
+name = input("What is your name? ")
+print(name)
+
+food = input("Whats your favourite food?")
+animal = input("Whats your favourite animal? ")
+
+print(f"{name}, you like {food} and {animal}")
+
